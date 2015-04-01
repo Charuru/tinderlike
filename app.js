692218b17d02a0ed7f34a470ee3bb37d114555d5
Charles <charuru2009@gmail.com>
2015-04-01 01:14:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var tinder = require('tinder/tinder');
 var settings = {
-    'facebook_token': '####', // Facebook Authorization Token
-    'facebook_id': '####' // Facebook User ID
+    'facebook_token': '', // Facebook Authorization Token
+    'facebook_id': ### // Facebook User ID
 };
 
 var client = new tinder.Tinder(settings['facebook_token'], settings['facebook_id']);
+// https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token
+
 
 function likeUser() {
     // Authenticate
@@ -23,7 +25,7 @@ function likeUser() {
 
             console.log('[LOG] Welcome back, '+ body.user.full_name);
 
-            setTimeout(likeUser, 10000);
+            setTimeout(likeUser, 2000);
         });
 
     // Queue Users
@@ -34,7 +36,7 @@ function likeUser() {
                 throw '[ERROR] Unable to fetch new users: '+ err;
             }
 
-            setTimeout(likeUser, 10000);
+            setTimeout(likeUser, 2000);
         });
 
     // Like User
@@ -51,7 +53,7 @@ function likeUser() {
                 }
             }
 
-            setTimeout(likeUser, 60000);
+            setTimeout(likeUser, 2000);
         });
     }
 }
@@ -66,4 +68,4 @@ app.get('/', function(req, res) {
         'queue': client.getUsers().length
     });
 });
-app.listen(80);
+app.listen(8080);
